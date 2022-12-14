e592d02ab215bc00d34b6951b419dddafa1cf69c
sudarshanreddyc <sudarshanreddy.c123@gmail.com>
2022-12-14 13:37:59 +0100

    Second commit

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,87 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Outlet, LiveReload, Link, Links, Meta, useLoaderData } from "@remix-run/react";
+import globalStylesUrl from '~/styles/global.css'
+// import { getUser } from '~/utils/session.server'
 
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const links = () => [{ rel: 'stylesheet', href: globalStylesUrl }]
+
+export const meta = () => {
+  const description = 'A cool blog built with Remix'
+  const keywords = 'remix, react, javascript'
+
+  return {
+    description,
+    keywords,
+  }
+}
+
+// export const loader = async ({ request }) => {
+//   const user = await getUser(request)
+//   const data = {
+//     user,
+//   }
+//   return data
+// }
 
 export default function App() {
   return (
-    <html lang="en">
+    <Document title={"Sample"}>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang='en'>
       <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
         <Meta />
         <Links />
+        <title>{title ? title : 'Remix Blog'}</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        {children}
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
-  );
+  )
+}
+
+function Layout({ children }) {
+  // const { user } = useLoaderData()
+
+  return (
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='logo'>
+          Remix
+        </Link>
+
+        <ul className='nav'>
+          <li>
+            <Link to='/posts'>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className='container'>{children}</div>
+    </>
+  )
+}
+
+export function ErrorBoundary({error}){
+  console.log(error)
+  return (
+    <Document>
+      <Layout>
+        <div>
+          <h1>Error</h1>
+          <p>{error.message}</p>
+        </div>
+      </Layout>
+    </Document>
+  )
 }
